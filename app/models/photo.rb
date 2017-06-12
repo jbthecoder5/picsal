69ef42ab00fa53cbd183100df3301e4da479d681
Joseph Bourne <kiratsuchi@live.com>
2017-06-12 12:31:19 -0700

    made a change to photo model so that upload cannot be send without presence of an image

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,7 @@
 class Photo < ApplicationRecord
 	validates :name, presence: true
 	validates :description, presence: true
+	validates :image, presence: true
 
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
